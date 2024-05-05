17be7602772b33d35c80dae1c08a5a2800125f8b
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-05-05 18:17:55 +0530

    Added:Document.CreateElement

---
+++ 01-create-element.js
@@ -0,0 +1,14 @@
+let newElement = document.createElement('input');
+let newElement1 = document.createElement('div');
+
+console.log(newElement)
+
+let ul = document.createElement('ul')
+let li = document.createElement('li')
+
+li.innerHTML += 'HELLO';
+ul.appendChild(li)
+
+document.body.appendChild(newElement)
+document.body.appendChild(li)
+
