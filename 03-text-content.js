96e5387ecd78ef77600e705e9147fc34f98f7d6d
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-05-05 22:39:50 +0530

    Added: textContent

---
+++ 03-text-content.js
@@ -0,0 +1,8 @@
+let textcontent = document.createElement('h1');
+textcontent.setAttribute('id','title');
+textcontent.innerHTML='Welcome to Code'
+
+console.log(textcontent);
+
+textcontent.textContent = 'Mango and Orange';
+
