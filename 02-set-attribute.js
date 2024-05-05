9001d19121cb53b32bdfb42ae5a6e2f5bac3bd6e
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-05-05 22:14:02 +0530

    Added: Document.SetAttribute()

---
+++ 02-set-attribute.js
@@ -0,0 +1,11 @@
+let inputbox = document.createElement('input');
+
+inputbox.setAttribute('type','text')
+inputbox.setAttribute('value','Nadeem Silawat')
+
+document.body.appendChild(inputbox) 
+
+const myElement = document.createElement('div');
+myElement.setAttribute('class','dcode');
+
+document.body.appendChild(myElement)
