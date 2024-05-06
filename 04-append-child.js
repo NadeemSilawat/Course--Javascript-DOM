53c12d404bb1d49a7214ee59d4947d13f63b3cc4
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-05-06 08:38:04 +0530

    appendChilds() Method

---
+++ 04-append-child.js
@@ -0,0 +1,21 @@
+const myList = document.getElementById('myList');
+
+//New List Item <li>Audi<li/>
+
+let newListItem = document.createElement('li');
+
+newListItem.textContent = 'Audi'
+myList.appendChild(newListItem)
+
+// Using Array 
+
+const fruitList = ['Mango','Apple','Orange','Lemon'];
+
+for(let fruit of fruitList){
+    let newItems = document.createElement('li')
+    newItems.textContent = fruit;
+
+    myList.appendChild(newItems);
+}
+
+
