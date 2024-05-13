556ec5b3774057dd14d52e2c13c73ddb86128dc2
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-05-13 21:51:53 +0530

    [200~const box = document.createElement('div');

---
+++ 11-get-bounding-client-rect.js
@@ -0,0 +1,13 @@
+const box = document.createElement('div');
+box.classList.add('box');
+document.body.appendChild(box);
+
+// when compelely show the box than chnage color
+window.addEventListener('scroll', function () {
+  if (box.getBoundingClientRect().bottom < window.innerHeight) {
+    box.classList.add('red-bg');
+  } else {
+    box.classList.remove('red-bg');
+  }
+  console.log(box.getBoundingClientRect().bottom, window.innerHeight);
+});
