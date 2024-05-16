8f686684c91a1c968cc5c6be2eae2c58681e3e65
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-05-16 17:09:53 +0530

    Append multiple child

---
+++ 19-append-multiple-child.js
@@ -0,0 +1,29 @@
+
+
+/* create this : <li    >[text]</li> */
+
+function createItems(text){
+    var li = document.createElement('li') ;
+
+    li.textContent = text;
+    return li
+
+}
+
+function appendChildren(parent , children){
+    children.forEach(child => {
+        parent.appendChild(child);
+    });
+}
+
+var myList = document.getElementById('myList')
+
+var item = [
+    createItems('Dom'),
+    createItems('Bom'),
+    createItems('Alex')
+]
+
+appendChildren(myList,item);
+
+console.log(myList);
