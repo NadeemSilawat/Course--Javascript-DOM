021461511d1ae6521c07ee8de8fcfe6d509e8ca6
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-05-21 17:06:34 +0530

    Added information of element selector

---
+++ 28-selecting-html-element.js
@@ -0,0 +1,10 @@
+/*
+!Selection Element
+
+  single element by id:           getElementById()
+  single element by CSS selector: querySelector()
+multiple element by CSS selector: querySelectorAll()
+multiple element by calss (live): getElementsByClassName()
+multiple element by tag (live):   getElementsByTabName()
+
+*/
